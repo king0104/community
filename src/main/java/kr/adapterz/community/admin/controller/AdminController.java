d806b50d925f13cfb0f7876199e191e8790e9f1a
4545abc <4545abc@naver.com>
2025-09-30 17:34:39 +0900

    feat: jwt 인증 인가 초기 구현 완료

---
+++ src/main/java/kr/adapterz/community/admin/controller/AdminController.java
@@ -0,0 +1,15 @@
+package kr.adapterz.community.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    @GetMapping
+    public String admin() {
+        return "admin controller";
+    }
+}
---
+++ src/main/java/kr/adapterz/community/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package kr.adapterz.community.auth.service;
+
+import kr.adapterz.community.member.entity.Member;
+import kr.adapterz.community.member.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        Member userData = memberRepository.findByEmail(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
