a79ab26bdefa75028e920de1ed6b86762c413ab2
4545abc <4545abc@naver.com>
2025-10-04 23:57:35 +0900

    fix: controller에 ResponseEntity로 반환하도록 변환

--- src/main/java/kr/adapterz/community/admin/controller/AdminController.java
+++ src/main/java/kr/adapterz/community/admin/controller/AdminController.java
@@ -1,5 +1,7 @@
 package kr.adapterz.community.admin.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin")
 public class AdminController {
 
+    // TODO: 정상적인 admin 로직 작성 (현재 테스트용 api)
     @GetMapping
-    public String admin() {
-        return "admin controller";
+    public ResponseEntity<String> admin() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("admin controller");
     }
 }
+
--- src/main/java/kr/adapterz/community/member/controller/MemberController.java
+++ src/main/java/kr/adapterz/community/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package kr.adapterz.community.member.controller;
 import kr.adapterz.community.member.dto.JoinRequest;
 import kr.adapterz.community.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public String join(@RequestBody JoinRequest joinRequest) {
+    public ResponseEntity<Void> join(@RequestBody JoinRequest joinRequest) {
         memberService.join(joinRequest);
 
-        return "ok";
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
     }
+
 }
