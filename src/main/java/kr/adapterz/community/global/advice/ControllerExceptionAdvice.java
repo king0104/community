de3fdba6061c4840458fd5a16a8d23aea1c8ff3b
Subeen Yoon <77609657+king0104@users.noreply.github.com>
2025-10-09 18:24:12 +0900

    feat: 예외 핸들러 추가 (#13)
    * feat: 커스텀 예외 추가
    
    * feat: 예외 핸들러 추가

---
+++ src/main/java/kr/adapterz/community/global/advice/ControllerExceptionAdvice.java
@@ -0,0 +1,26 @@
+package kr.adapterz.community.global.advice;
+
+import kr.adapterz.community.global.dto.ExceptionResponse;
+import kr.adapterz.community.global.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionAdvice {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponse> handleCustomException(CustomException e) {
+        log.warn("기본 예외 발생");
+        return ResponseEntity.
+                status(e.getErrorCode().getStatus())
+                .body(ExceptionResponse.fail(
+                        e.getErrorCode().getStatus().value(),
+                        e.getErrorCode().getMessage()
+                ));
+    }
+
+}
