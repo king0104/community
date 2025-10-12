cb7db2dacc2ce551a107af049a5873e24c9ad09b
4545abc <4545abc@naver.com>
2025-10-13 05:23:45 +0900

    feat: 회원정보 조회 API 개발

--- src/main/java/kr/adapterz/community/auth/service/CustomUserDetailsService.java
+++ src/main/java/kr/adapterz/community/auth/service/CustomUserDetailsService.java
@@ -2,6 +2,8 @@ package kr.adapterz.community.auth.service;
 
 import kr.adapterz.community.domain.member.entity.Member;
 import kr.adapterz.community.domain.member.repository.MemberRepository;
+import kr.adapterz.community.global.exception.ErrorCode;
+import kr.adapterz.community.global.exception.NotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,14 +22,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         //DB에서 조회
-        Member userData = memberRepository.findByEmail(username);
+        Member member = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
 
-        if (userData != null) {
-
-            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
-            return new CustomUserDetails(userData);
-        }
-
-        return null;
+        //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+        return new CustomUserDetails(member);
     }
+
 }
